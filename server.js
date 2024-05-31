f82e180cb12ee4fa5ffb3e1234b052d96d19de91
Kam <erkamyaman35@gmail.com>
2024-05-31 22:31:27 +0300

    docker-build

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 
 const app = express()
-const PORT = 3000;
 
 
 app.get('/', (req, res) => {
     res.send('hello it is the get request')
 })
 
-app.listen(PORT, () => {
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log(`listening to ${PORT}`)
 })
