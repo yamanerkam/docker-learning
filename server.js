70a2c2c16c45d37fac403c698241a86e90b09837
Kam <erkamyaman35@gmail.com>
2024-06-02 23:55:02 +0300

    new-get-req-id

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/docker', (req, res) => {
     res.send('docker get req')
 })
 
+app.get('/docker/:id', (req, res) => {
+    res.send(`get req id: ${req.params.id}`)
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
